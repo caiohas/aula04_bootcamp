9d27174e50795d79051bcaecfae6b104bfa026c1
Caio Souza <caiohas.2908@gmail.com>
2025-01-23 00:19:14 -0300

    adicionando funcao que ordena e invocando ela em outro arquivo data_science.py

--- aula_algoritmo.py
+++ aula_algoritmo.py
@@ -1,17 +1,23 @@
 ## Algoritmo de Ordenação Bubble Sort
 
-lista_numeros = [40,50,60,70,0,-408593,1,50]
+# lista_numeros = [40,50,60,70,0,-408593,1,50]
+# lista_numeros_02 = [40,60,27,0,-4,15,50]
+# lista_numeros_03 = [29,25,8,702,10,-403,19,550]
 
+def ordena_lista_numeros(numeros) -> list:
+    nova_lista_numeros = numeros.copy()
 
-def ordena_numeros(numeros):
-    nova_lista_numeros = lista_numeros.copy()
-
-    for i in range(len(lista_numeros)):
-        for j in range(i+1, len(lista_numeros)):
-            if lista_numeros[i] > lista_numeros[j]: # se numero na posicao i > numero na posicao j
-                lista_numeros[i], lista_numeros[j] = lista_numeros[j], lista_numeros[i] # realiza a troca
+    for i in range(len(nova_lista_numeros)):
+        for j in range(i+1, len(nova_lista_numeros)):
+            if nova_lista_numeros[i] > nova_lista_numeros[j]: # se numero na posicao i > numero na posicao j
+                nova_lista_numeros[i], nova_lista_numeros[j] = nova_lista_numeros[j], nova_lista_numeros[i] # realiza a troca
     
     return nova_lista_numeros
 
-nova_lista = ordena_numeros(lista_numeros)
-print(lista_numeros)
+# nova_lista = ordena_lista_numeros(lista_numeros)
+# nova_lista_02 = ordena_lista_numeros(lista_numeros_02)
+# nova_lista_03 = ordena_lista_numeros(lista_numeros_03)
+
+# print(lista_numeros)
+# print(lista_numeros_02)
+# print(lista_numeros_03)
---
+++ data_science.py
@@ -0,0 +1,6 @@
+from aula_algoritmo import ordena_lista_numeros
+
+lista = [13,5,-9,8,45,92,501,-29]
+
+nova_lista = ordena_lista_numeros(lista)
+print(nova_lista)
